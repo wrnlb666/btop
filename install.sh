459243732cb5817003f387706b46738bfb2d4040
wrnlb666 <wrnlb666@gmail.com>
2026-03-07 13:34:10 +1100

    add catppuccin themes to collection

--- install.sh
+++ install.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
 [[ -d ~/.config/btop ]] || mkdir -p ~/.config/btop
+[[ -d ~/.config/btop/themes ]] || mkdir -p ~/.config/btop/themes
+
 ln -sf "$(pwd)/btop.conf" ~/.config/btop/
+
+for fi in themes/*.theme; do
+    theme="$(basename "$fi")"
+    ln -sf "$(pwd)/${fi}" "${HOME}/.config/btop/themes/${theme}"
+done
